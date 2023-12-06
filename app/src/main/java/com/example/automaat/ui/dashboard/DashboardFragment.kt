11405127dae3f71087d7ea612b85660d1516d4a6
leonbos1 <ktmleon@live.nl>
2023-12-06 12:14:15 +0100

    Swapped Home and Dashboard in nav

--- app/src/main/java/com/example/automaat/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/automaat/ui/dashboard/DashboardFragment.kt
@@ -7,35 +7,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.automaat.databinding.FragmentDashboardBinding
+import com.example.automaat.databinding.FragmentReservationsBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+private var _binding: FragmentReservationsBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    _binding = FragmentReservationsBinding.inflate(inflater, container, false)
+    val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    val textView: TextView = binding.textDashboard
+    dashboardViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
     }
+    return root
+  }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
